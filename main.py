2cf9a010d539820a44133fdeb75fc4ebe55befb3
avig-apiiro <avi.glatstein@apiiro.com>
2025-03-13 08:37:49 +0200

    test version

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ def get_facebook_data2(endpoint, access_token):
 if __name__ == '__main__':
     data = get_facebook_data('user', "None")
     print(data)
-    if (data == 10):
+    if (data == 11):
         get_facebook_data2("/user", "")
