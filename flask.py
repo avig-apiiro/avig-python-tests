0607b936ef43cbfa2cb40a7c762023f10e5f156e
avig-apiiro <avi.glatstein@apiiro.com>
2025-06-01 11:25:14 +0300

    wip

--- flask.py
+++ flask.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/user")
+def get_user():
+    return json.dumps(get_user())
---
+++ user.py
@@ -0,0 +1,2 @@
+def get_user():
+    return {'userId': 1, 'userName': 'm', 'userEmail': 'm@mi5.com'}
